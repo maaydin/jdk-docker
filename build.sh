4aec046af1c5f34d8531e9a224c10aaf6babcf0d
maaydin <maaydin@gmail.com>
2016-06-28 10:32:43 +0300

    Docker file added

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker build --rm -t maaydin/centos-7-oracle-jdk-8:8.91 -t maaydin/centos-7-oracle-jdk-8:latest .
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run -it --rm maaydin/centos-7-oracle-jdk-8:latest
